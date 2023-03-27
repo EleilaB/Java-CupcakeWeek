6d2abf6449f092562cc0e3828f601aedf06f5fa4
Eleila Bradford <eleila.bradford@gmail.com>
2023-03-27 13:30:38 -0600

    Order class completed

--- src/Order.java
+++ src/Order.java
@@ -41,7 +41,32 @@ public class Order {
                 System.out.println("What would you like to order? Please use the number associated with each item to order.");
                 int orderChoice = scan.nextInt();
                 scan.nextLine();
-                // !!! put more stuff here !!!
+                if(orderChoice == 1){
+                    order.add(cupcakeMenu.get(0));
+                    System.out.println("Item added to order.");
+                }else if(orderChoice == 2){
+                    order.add(cupcakeMenu.get(1));
+                    System.out.println("Item added to order.");
+                }else if(orderChoice == 3){
+                    order.add(cupcakeMenu.get(2));
+                    System.out.println("Item added to order.");
+                }else if(orderChoice == 4){
+                    order.add(drinkMenu.get(0));
+                    System.out.println("Item added to order.");
+                }else if(orderChoice == 5){
+                    order.add(cupcakeMenu.get(1));
+                    System.out.println("Item added to order.");
+                }else if(orderChoice == 6){
+                    order.add(cupcakeMenu.get(2));
+                    System.out.println("Item added to order.");
+                }else{
+                    System.out.println("Sorry, we don't seem to have that on the menu.");
+                }
+                System.out.println("\nWould you like to continue ordering?");
+                placeOrder = scan.nextLine();
+                if(!placeOrder.equalsIgnoreCase("Y")){
+                    ordering = false;
+                }
             }
         } else {
             System.out.println("Have a nice day then.");
